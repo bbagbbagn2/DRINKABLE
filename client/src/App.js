10357f8d7b09dd7ed3e706463ed159fba70c90be
bbagbbagn2 <pyoungh999@naver.com>
2023-05-25 16:16:30 +0900

    remove(AmountPage) AmountPage 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainPage from "./MainPage";
 import FlavorPage from "./FlavorPage";
-import AmountPage from "./AmountPage";
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<MainPage/>} />
         <Route exact path="/flavor" element={<FlavorPage/>} />
-        <Route exact path="/amount" element={<AmountPage/>} />
       </Routes>
     </Router>
   );
