c424a6b0ce7dfc0e9da6f57323c2f163cbbca4a9
bbagbbagn2 <pyoungh999@naver.com>
2023-05-25 13:30:29 +0900

    feat(ItemWrapper) ItemWrapper 추가

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -31,11 +31,30 @@ export default function Main() {
                         </TitleWrapper>
                         <AboutContainer>
                             <AboutWrapper>
-                                <About>A special website for introductory cocktails</About>
-                                <X>X</X>
+                                <AboutTitleWrapper>
+                                    <AboutTitle>A special website for introductory cocktails</AboutTitle>
+                                </AboutTitleWrapper>
+                                <X margintop="55px">X</X>
                                 <IntroduceWrapper>
                                     <Introduce>Our website is a space created to introduce cocktails and provide new cocktail experiences. We provide a variety of cocktail recipes and trendy drink trends to help users freely explore cocktails. Our goal is to share cocktail culture with cocktail lovers by making it more diverse and exciting. Our website also provides information on cocktail ingredients. Welcome to the ultimate destination for cocktail lovers and new discoveries.</Introduce>
                                 </IntroduceWrapper>
+                                <AboutImageWrapper>
+                                    <ItemWrapper>
+                                        <ItemLinkWrapper>
+                                            <AboutTitleWrapper>
+                                                <AboutTitle>The Cocktails</AboutTitle>
+                                            </AboutTitleWrapper>
+                                            <X>X</X>
+                                            <ItmeLink></ItmeLink>
+                                        </ItemLinkWrapper>
+                                    </ItemWrapper>
+                                    <ItemWrapper>
+
+                                    </ItemWrapper>
+                                    <ItemWrapper>
+
+                                    </ItemWrapper>
+                                </AboutImageWrapper>
                             </AboutWrapper>
                         </AboutContainer>
                     </Wrapper>
@@ -107,7 +126,6 @@ const Message = styled(Title)`
 
 const AboutContainer = styled.div`
     margin-top: 99px;
-    padding: 0 18%;
     height: 1100px;
 `;
 
@@ -115,8 +133,11 @@ const AboutWrapper = styled.div`
     display: grid;
     justify-items: center;
 `;
+const AboutTitleWrapper = styled.div`
+    margin: 0 38%;
+`;
 
-const About = styled.h2`
+const AboutTitle = styled.h2`
     margin: 0;
     text-align: center;
     line-height: 1.25em;
@@ -125,21 +146,50 @@ const About = styled.h2`
 `;
 
 const X = styled.p`
-    margin-top: 54px;
     margin-bottom: 0;
     line-height: 1.4em;
     font-size: 16px;
     color: #8E6C62;
+    
+    margin-top: ${(props) => (props.margintop || "0")};
 `;
 
 const IntroduceWrapper = styled.div`
-    margin: 0 16%;
-`; 
+    margin: 0 30%;
+`;
 
 const Introduce = styled.p`
-    margin-top: 49px;
+    margin-top: 50px;
     text-align: center;
     line-height: 1.7em;
     font-size: 16px;
     color: #8E6C62;
+`;
+
+const AboutImageWrapper = styled.div`
+    margin-top: 70px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+`;
+
+const ItemWrapper = styled.div`
+    width: 307px;
+    height: 454px;
+`;
+
+const ItemLinkWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
+    border-top: 1px solid #8E6C62;
+    border-bottom: 1px solid #8E6C62;
+`;
+
+const ItmeLink = styled.div`
+    width: 110px;
+    height: 38px;
+    background: #8E6C62;
 `;
