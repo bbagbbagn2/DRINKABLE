cf615b3ae09ab916f435a80e0fab0cffd662c524
bbagbbagn2 <pyoungh999@naver.com>
2024-02-22 09:50:37 +0900

    fix: 코드 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Header from '../../layouts/Header';
 import Footer from '../../layouts/Footer';
 import AllergySection from './component/AllergySection';
 import BannerSection from './BannerSection/BannerSection';
 import CampaingnSection from './component/CampaignSection';
-import FlavorSection from './FlavorSection/FlavorSection';
-import BlueSection from './BlueSection/BlueSection';
-import ExploreSection from './ExploreSection/ExploreSection';
 
 export default function Main() {
     return (
