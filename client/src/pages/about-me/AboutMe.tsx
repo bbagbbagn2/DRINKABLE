3ea1d878855288a8aefb500d9a5e63c933bcc4ba
bbagbbagn2 <pyoungh999@naver.com>
2024-03-08 02:06:36 +0900

    text 파일 생성

--- client/src/pages/about-me/AboutMe.tsx
+++ client/src/pages/about-me/AboutMe.tsx
@@ -12,18 +12,29 @@ export default function BannerSection() {
       <Header />
       <main className="container responsivegrid">
         <div className="cmp-container">
-          <TeaserHero src={main1}>Welcome to JIHUN</TeaserHero>
+          <TeaserHero src={main1}>ABOUT ME</TeaserHero>
+          <Title>
+          <h1 className="cmp-title__text">Welcome to JIHUN</h1>
+          </Title>
           <Title>
             <h1 className="cmp-title__text">Profile</h1>
           </Title>
-          <div className="teaser hero hero--normal content-on-top content-on-left">
-            <div className="cmp-teaser">
-              <div className="cmp-teaser__image">
-                <div className="cmp-image">
-                  <picture className="loaded visible">
-                  </picture>
-                </div>
-              </div>
+          <div className="container responsivegrid grid two-columns">
+            <div className="cmp-container">
+              <SkillCard title="Profile" src={main1}>
+                박지훈
+                <hr />
+                1999.01.13
+                <hr />
+                Busan, South Korea
+                <hr />
+              </SkillCard>
+              <SkillCard title="Contact" src={main1}>
+                010-9207-8758
+                <hr />
+                pyoungh137@gamil.com
+                <hr />
+              </SkillCard>
             </div>
           </div>
           <div className="container responsivegrid">
@@ -36,9 +47,9 @@ export default function BannerSection() {
                   <SkillCard title="Fron-End" src={main1}>
                     HTML, CSS, JS, TS
                     <hr />
-                    React, styled-components
+                    React, Next.js, Axios 
                     <hr />
-                    Axios
+                    styled-components
                     <hr />
                   </SkillCard>
                   <SkillCard title="Back-End" src={main1}>
@@ -73,7 +84,12 @@ export default function BannerSection() {
                     대학시절부터 현재까지 진행한 프로젝트와 TIL 등 다양한
                     소스코드 저장소입니다.
                   </SkillCard>
-                  <SkillCard title="Pjh-Portfolio" src={main1} showActionLink={true} href="https://pjh-portfolio.netlify.app/">
+                  <SkillCard
+                    title="Pjh-Portfolio"
+                    src={main1}
+                    showActionLink={true}
+                    href="https://pjh-portfolio.netlify.app/"
+                  >
                     프론트엔드를 학습한 초창기 때 만든 웹 포트폴리오입니다.
                   </SkillCard>
                   <SkillCard title="Communication" src={main1}>
