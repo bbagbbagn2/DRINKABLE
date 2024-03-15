404861e47d155dc9908f351e04c420c8bd90792b
bbagbbagn2 <pyoungh999@naver.com>
2024-03-15 10:27:29 +0900

    ICONDB 글 수정

--- client/src/pages/projects/icondb.tsx
+++ client/src/pages/projects/icondb.tsx
@@ -38,15 +38,41 @@ export default function Icondb() {
               <div className="text">
                 <div className="cmp-text">
                   <p>
-                    평소 대학 과제 발표 시 제작하는 PPT에 사용되는 아이콘을
-                    찾으면서 기존 아이콘 다운로드 사이트는 과도한 광고 및
-                    유료화로 인해 접근이 힘들었습니다.&nbsp; 또한, 아이콘
-                    이미지의 색상 및 모양 등 제가 사용하는 목적에 완벽히
-                    부합하는 아이콘을 찾는 것에 어려움이 있었습니다.&nbsp;
-                    이러한 문제점들을 개선하기 위해 웹사이트 내에서 아이콘을
-                    직접 변경, 수정할 수 있는 웹서비스를 팀원들과
-                    개발했습니다.&nbsp; 개발 구성원은 프론트엔드 개발자 1명,
-                    백엔드 개발자 2명으로 구성되어 있습니다.
+                    ICONDB는 무료로 아이콘을 다운로드할 수 있는 웹
+                    서비스입니다.&nbsp;저희는 기존 아이콘 다운로드 사이트의
+                    불편한 점을 찾고 이를 개선하여 사용자에게 더 편리한 웹
+                    서비스를 만들고 싶었습니다.&nbsp;사용자의 목적에 완전히
+                    일치하는 아이콘을 만들기 위해 기존의 사이트와는 다르게 ICODB
+                    내에서 직접 수정할 수 있도록 했습니다.
+                  </p>
+                </div>
+              </div>
+              <div className="text">
+                <div className="cmp-text">
+                  <p>
+                    저는 이 프로젝트를 진행하면서 프론트엔드 개발자의 역할 뿐만
+                    아니라 페이지 디자인, 팀원의 일정 관리 등 설계부터 개발 및
+                    배포까지 다양한 경험을 쌓을 수 있었습니다.
+                  </p>
+                </div>
+              </div>
+              <div className="text">
+                <div className="cmp-text">
+                  <p>
+                    저는 프론트엔드 개발자로서 다양한 기능을 개발했습니다.
+                    <br />
+                    사용자 관련 페이지에서 로그인 및 회원가입 기능을 개발했고,
+                    후에 사용자의 편의성을 고려해 소셜 로그인 기능을
+                    추가했습니다. 프로필 페이지에서는 사용자만의 닉네임과 프로필
+                    사진을 추가할 수 있도록 설정했습니다.
+                    <br />
+                    그리고 아이콘 검색 시 제목 및 사용자, 해시태그 등 다양한
+                    필터를 사용해 원하는 아이콘을 찾을 수 있도록 했습니다.
+                    <br />
+                    아이콘 상세 페이지에 좋아요 기능을 추가해 자신만의
+                    위시리스트를 만들어 좋아요를 누른 아이콘을 한번에 볼 수
+                    있도록 했습니다. 또한, 팔로우 기능을 추가해 팔로우한
+                    사용자의 아이콘을 쉽게 찾을 수 있습니다.
                   </p>
                 </div>
               </div>
@@ -87,32 +113,38 @@ export default function Icondb() {
                   </SkillCard>
                 </div>
               </div>
+              
             </div>
           </div>
           <div className="container responsivegrid">
             <div className="cmp-container">
               <Title>
-                <h2 className="cmp-title__text">Key Features</h2>
+                <h2 className="cmp-title__text" />
               </Title>
               <div className="text">
                 <div className="cmp-text">
                   <p>
-                    사용자 관련 기능으로 로그인 및 회원가입 기능을 개발했고,
-                    사용자의 편의성을 고려해 소셜 로그인 기능 또한
-                    추가했습니다.&nbsp; 제목 및 사용자, 해시태그를 이용한 다양한
-                    필터로 검색이 가능합니다.&nbsp; 좋아요 를 눌러 자신만의
-                    위시리스트를 만들어 좋아요를 누른 아이콘을 한번에 볼 수
-                    있습니다.&nbsp; 사용자 간 팔로우를 통해 팔로우한 사용자의
-                    아이콘을 쉽게 찾을 수 있습니다.&nbsp; 프로필 페이지에서
-                    사용자의 닉네임과 프로필 사진을 설정할 수 있습니다.
+                    대학교 졸업작품을 위해 약 3개월 동안 개발한 ICONDB를 통해 많은 경험을 했습니다. 
+                    <br />위 프로젝트를 진행하면서 React와 styled-components를
+                    학습할 수 있었고, 이를 통해 기존에 사용했던 HTML / CSS를
+                    React로 전환할 수 있었습니다. 또한, 첫 프론트엔드 프로젝트이다
+                    보니 많은 기술적 문제에 직면하며 시행착오도 있었지만, 위 문제를 하나씩 해결하면서 제 개발 지식을
+                    향상시킬 수 있었습니다.
+                    <br />
+                    기술적 지식 뿐만 아니라, 팀원과의 협업 시 발생한 의사소통
+                    문제에서도 많은 개선점을 확인할 수 있었습니다. Notion을
+                    사용한 일정 관리로 프로젝트 완성 기간을 차질없이 맞출 수 있었고, 정기
+                    회의 시 회의록을 작성 해 꾸준한 개발 완성도를 유지할 수
+                    있었습니다.
                   </p>
                 </div>
               </div>
             </div>
           </div>
+          
           <CampaingnCard
             className="content-on-left"
-            title="자세한 ICONDB 정보"
+            title="ICONDB Information"
             description="팀원과 함께 개발한 ICONDB 개발 과정을 기록하였습니다."
             src={icondb1}
           >
@@ -120,8 +152,8 @@ export default function Icondb() {
           </CampaingnCard>
           <CampaingnCard
             className="content-on-right"
-            title="개발 회고록"
-            description="프로젝트 개발 시 발생한 문제점 및 해결 과정, 리패토링을 회고록에 작성했습니다."
+            title="ICONDB 개발 회고록"
+            description="프로젝트 개발 시 발생한 기능적 문제 및 해결 과정, 리패토링을 회고록에 작성했습니다."
             src={icondb2}
           >
             <Button buttonText="회고록 보기" />
